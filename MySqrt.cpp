84d8279ee4a24a1a4305828bbf4364fc6d22bba8
Jordi Frittoli <jordi.frittoli19@imperial.ac.uk>
2023-03-21 16:39:18 +0000

    added good

---
+++ MySqrt.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jf1519 on 21/03/23.
+//
+
+#include "MySqrt.h"
---
+++ MySqrt.h
@@ -0,0 +1,14 @@
+//
+// Created by jf1519 on 21/03/23.
+//
+
+#ifndef SQRT_MYSQRT_H
+#define SQRT_MYSQRT_H
+
+
+class MySqrt {
+
+};
+
+
+#endif //SQRT_MYSQRT_H
